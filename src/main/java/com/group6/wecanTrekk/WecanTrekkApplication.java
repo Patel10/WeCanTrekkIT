26e11ba42efaa596a389479de6edc233a6a52e61
Patel10 <parth1029@yahoo.com>
2021-06-10 15:13:37 -0400

    first commit

---
+++ src/main/java/com/group6/wecanTrekk/WecanTrekkApplication.java
@@ -0,0 +1,13 @@
+package com.group6.wecanTrekk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecanTrekkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WecanTrekkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/wecanTrekk/WecanTrekkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.wecanTrekk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecanTrekkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
