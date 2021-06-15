831b5c4f8b8a4f25662dfd4200d9804a3dfdfd88
Shellejd <rochelle.dishmon@gmail.com>
2021-06-15 11:32:20 -0400

    Added continents & regions -need real populator

---
+++ src/main/java/com/group6/wecanTrekk/ContinentRepository.java
@@ -0,0 +1,14 @@
+package com.group6.wecanTrekk;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ContinentRepository extends CrudRepository<Continent, Long>{
+
+    List<Continent> findByName(String name);
+
+    Continent findById(long id);
+
+}
---
+++ src/main/java/com/group6/wecanTrekk/Region.java
@@ -0,0 +1,68 @@
+package com.group6.wecanTrekk;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name; //regionName
+    private String location;
+
+    @ManyToOne
+    private Continent continent;
+
+
+    public Long getId() {
+
+        return id;
+    }
+
+   // public Collection<Continent> getContinents() {
+    //   return continents;
+  //  }
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getLocation() {
+
+        return location;
+    }
+    @Override
+    public String toString() {
+        return String.format("Region[id=%d, name=%s, location=%s]", id, name,location);
+    }
+
+    protected Region() {
+    }
+
+    public Region(String name, String location, Continent...continents) {
+        this.name = name;
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Region region = (Region) o;
+        return Objects.equals(id, region.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
+
