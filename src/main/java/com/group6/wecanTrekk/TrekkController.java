5530ff3730ebf7f9101966eb1c4f4d866f80c994
Patel10 <parth1029@yahoo.com>
2021-06-17 18:41:09 -0400

    trektemplate and css in works

--- src/main/java/com/group6/wecanTrekk/TrekkController.java
+++ src/main/java/com/group6/wecanTrekk/TrekkController.java
@@ -4,6 +4,7 @@ package com.group6.wecanTrekk;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 
@@ -17,11 +18,12 @@ public class TrekkController {
     private TrekkRepository trekkRepo;
 
 
-    @RequestMapping("/trekk")
+    @RequestMapping("/trek")
     public String displayAllTrekks (Model model){
         model.addAttribute("TrekkModel",trekkRepo.findAll());
         model.addAttribute("RegionsModel",regionRepo.findAll());
-        return "TrekkView";
+        model.addAttribute("ContinentModel", continentRepo.findAll());
+        return "trekView";
 
     }
 }
