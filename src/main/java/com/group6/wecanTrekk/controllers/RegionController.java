ff5498e54de8c7aa1841217f66b9d2630f5cbeda
Shellejd <rochelle.dishmon@gmail.com>
2021-06-17 19:42:15 -0400

    push RegionControllerChanges

---
+++ src/main/java/com/group6/wecanTrekk/controllers/RegionController.java
@@ -0,0 +1,27 @@
+package com.group6.wecanTrekk.controllers;
+
+
+import com.group6.wecanTrekk.repositories.RegionRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class RegionController {
+    @Resource
+    private RegionRepository regionRepo;
+
+    public String findAllRegions(Model model) {
+        model.addAttribute("regionsModel", regionRepo.findAll());
+        return "regionsTemplate";
+
+    }
+    @RequestMapping("/region/{id}")
+    public String findOneRegion(Model model, @PathVariable Long id){
+        model.addAttribute("regionModel", regionRepo.findAll());
+        return "regionTemplate";
+    }
+}
